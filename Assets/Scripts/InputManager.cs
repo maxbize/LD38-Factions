d828a2a22f240ce216d87edbe716b1ba7a1ea8a3
Frenchie14 <mbize2005@gmail.com>
2017-04-21 22:01:13 -0700

    Fix selection bug

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -21,7 +21,7 @@ public class InputManager : MonoBehaviour {
         if (selectedPawns.Count == 0) {
             if (Input.GetMouseButtonDown(LEFT_CLICK)) {
                 startPoint = Input.mousePosition;
-            } else if (Input.GetMouseButtonUp(LEFT_CLICK)) {
+            } else if (Input.GetMouseButtonUp(LEFT_CLICK) && startPoint != Vector2.zero) {
                 SelectUnits(startPoint, Input.mousePosition);
             }
         } else if (Input.GetMouseButtonDown(LEFT_CLICK)) {
@@ -30,8 +30,10 @@ public class InputManager : MonoBehaviour {
             if (Physics.Raycast(ray, out hit)) {
                 foreach (Pawn pawn in selectedPawns) {
                     pawn.SetTargetPos(hit.point);
+                    pawn.GetComponent<Renderer>().material.color = Color.red;
                 }
-                selectedPawns.Clear();    
+                selectedPawns.Clear();
+                startPoint = Vector2.zero;
             }
         }
 	}
@@ -46,8 +48,9 @@ public class InputManager : MonoBehaviour {
         foreach (Pawn pawn in allPawns) {
             Vector2 pawnScreenPos = Camera.main.WorldToScreenPoint(pawn.transform.position);
             if (pawnScreenPos.x > topLeft.x && pawnScreenPos.x < botRight.x && pawnScreenPos.y > botRight.y && pawnScreenPos.y < topLeft.y) {
-                Debug.Log(pawn.gameObject + " in selection");
                 selectedPawns.Add(pawn);
+                pawn.GetComponent<Renderer>().material.color = Color.magenta; // Temp hack? :)
+                // TODO: Filter out enemy pawns, pawns on other side of planet
             }
         }
     }
